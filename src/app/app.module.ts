a719f9e3ea742e12519872aaa033833b5106d2ac
ZINE Mahmoud <mr.zine.mahmoud@gmail.com>
2019-06-13 11:30:11 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NbThemeModule, NbLayoutModule, NbInputModule, NbCardModule} from '@nebular/theme';
+import {NbCardComponent} from '@nebular/theme/components/card/card.component';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'default' }),
+    NbLayoutModule,
+    NbInputModule,
+    NbCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
